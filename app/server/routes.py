3e5af4a3088c4272c0e135225b00ab34f713ba31
Erik Andreasson <erikandreasson@MacBook-Air-som-tillhor-Erik.local>
2026-06-24 15:18:21 +0200

    Flyttat dashboard

--- app/server/routes.py
+++ app/server/routes.py
@@ -73,12 +73,18 @@ def index():
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/models')
-def run_models_page():
-    return render_template('run_models.html')
+@app.route('/companies')
+def companies():
+    return render_template('companies.html')
 
 @app.route('/company/<ticker>')
 def company(ticker):
     return render_template('company.html', ticker=ticker)
+
+@app.route('/models')
+def run_models_page():
+    return render_template('run_models.html')
+
+
 
 
--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from app.server.models.model_extras import compute_database_zscores, print_outli
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        print_outlier_summary(df=compute_database_zscores())
+    # with app.app_context():
+    #     print_outlier_summary(df=compute_database_zscores())
     app.run(port=5001, debug=True)
